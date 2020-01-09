6e0415580eccf4f07f144b1b7d1d2a32bacfbd7e
Sarfaraz Ahmed <imsarfy@gmail.com>
2020-01-09 16:04:31 +0530

    formset configuration added

---
+++ tform/static/js/main.js
@@ -0,0 +1,7 @@
+$('.formset_row').formset({
+  addText: 'Add Choice',
+  addCssClass: 'btn btn-block btn-outline-success',
+  deleteText: 'Remove Choice',
+  deleteCssClass: 'btn btn-block btn-outline-danger',
+  prefix: 'mcqchoicefield_set',
+});
